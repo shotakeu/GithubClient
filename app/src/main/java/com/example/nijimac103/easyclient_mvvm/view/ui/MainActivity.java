2760d60020616c9d6f2807d2e8e4f2c2e5b6b1fb
Tsutou Takehara <tsutoutakehara@gmail.com>
2018-05-25 12:30:26 +0900

    initial commit

---
+++ app/src/main/java/com/example/nijimac103/easyclient_mvvm/view/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.nijimac103.easyclient_mvvm.view.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nijimac103.easyclient_mvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
